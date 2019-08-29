ae95179de270bc87741510657119718cf542fe36
Elizabeth Peters <eliznyc@gmail.com>
2019-08-29 03:30:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,13 +1,12 @@
+function forLoop(array) {
+for (let i = 0; i < 25; i++) {array.push(`I am ` + i + ` strange loop` + (i===1 ? "." : "s.")) }
+  return array;
+}
+
 function whileLoop(n) {
     while (n > 0) {console.log(--n);
                   }
     return 'done';
-}
-
-function forLoop(array) {
-    var array = []
-	for (let i = 0; i < 25; i++) {array.push(`I am ` + i + ` strange loop` + (i===1 ? "." : "s.")) }
-    return array;
 }
 
 function doWhileLoop(n) {
